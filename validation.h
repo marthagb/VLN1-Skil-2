b08fee4dfbb6cd20e82e0ce3046ceafecfff5e64
Jakob Örn <jakobog16@ru.is>
2016-12-09 18:14:17 +0000

    comment í validation, eyddi 'dauð' föll

--- validation.h
+++ validation.h
@@ -10,10 +10,10 @@ class validation
 
 public:
     validation();
+
     bool validName(const string& s);
     int birthChecks(int birthYear, int deathYear);
     bool genderCheck(char& gender);
-    bool errorCheckSaveToFile();
     bool validYear(const string& s, int& year);
     bool validComputerName (const string& n);
     bool validComputerType(const string& s);
